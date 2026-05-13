cf93cd6d2138de3c860219ff1b58f1e39d6be5d4
CipherJayesh-hub <predatorgehlot2023@gmail.com>
2026-05-13 14:05:29 +0530

    data type change

--- CRT_FOP_DSA/src/Recursion/Q4.java
+++ CRT_FOP_DSA/src/Recursion/Q4.java
@@ -8,13 +8,13 @@ public class Q4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number : ");
-        int num = sc.nextInt();
-        int res = sum(num);
+        long num = sc.nextInt();
+        long res = sum(num);
         System.out.println(s);
 
     }
-static int s= 0;
-    public static int sum(int n){
+static long s= 0;
+    public static int sum(long n){
         if(n==0) return 0;
         s = sum += n%10;
         return sum(n/10);
---
+++ CRT_FOP_DSA/src/Stack_Queue/Q1.java
@@ -0,0 +1,7 @@
+package Stack_Queue;
+
+public class Q1 {
+    public static void main(String[] args) {
+
+    }
+}
