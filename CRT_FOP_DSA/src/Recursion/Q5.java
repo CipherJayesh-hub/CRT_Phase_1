2a8de2bb9633ff99b42bfb7ba6e56d7c42d92cef
CipherJayesh-hub <predatorgehlot2023@gmail.com>
2026-05-13 14:45:28 +0530

    one file add in recursion

---
+++ CRT_FOP_DSA/src/Recursion/Q5.java
@@ -0,0 +1,19 @@
+package Recursion;
+
+
+import java.util.Scanner;
+
+public class Q5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.println(pow(num)); 
+
+    }
+
+    public static boolean pow(int n){
+        if(n==0) return false;
+        if(n==1) return true;
+        return (n%2==0) && pow(n/2);
+    }
+}
