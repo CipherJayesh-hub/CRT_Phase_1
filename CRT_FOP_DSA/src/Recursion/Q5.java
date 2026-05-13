1bd45a9a6a61de0679d0a5e8a1a178b9371e3447
CipherJayesh-hub <predatorgehlot2023@gmail.com>
2026-05-13 14:58:48 +0530

    add more ques

--- CRT_FOP_DSA/src/Recursion/Q5.java
+++ CRT_FOP_DSA/src/Recursion/Q5.java
@@ -7,7 +7,7 @@ public class Q5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
-        System.out.println(pow(num)); 
+        System.out.println(pow(num));
 
     }
 
@@ -15,5 +15,17 @@ public class Q5 {
         if(n==0) return false;
         if(n==1) return true;
         return (n%2==0) && pow(n/2);
+    }
+
+    public static boolean pow(int n){
+        if(n==0) return false;
+        if(n==1) return true;
+        return (n%3==0) && pow(n/3);
+    }
+
+    public static boolean pow(int n){
+        if(n==0) return false;
+        if(n==1) return true;
+        return (n%4==0) && pow(n/4);
     }
 }
